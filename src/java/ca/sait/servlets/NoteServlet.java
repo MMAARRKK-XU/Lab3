5ac43fd835931886a2ff7f75188cb6bb7ff5f51b
MMAARRKK-XU <xu9251525@gmail.com>
2022-09-27 20:06:04 -0600

    Display the note from the file

--- src/java/ca/sait/servlets/NoteServlet.java
+++ src/java/ca/sait/servlets/NoteServlet.java
@@ -1,7 +1,7 @@
 package ca.sait.servlets;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import ca.sait.models.Note;
+import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,6 +25,25 @@ public class NoteServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        // to read files
+         BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+         
+         String title = br.readLine();
+         String contents = br.readLine();
+        
+        Note note = new Note(title, contents);
+        
+        request.setAttribute("note", note);
+        
+        String edit = request.getParameter("edit");
+        
+        if(edit != null){
+            this.getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp").forward(request, response);
+        } else {
+            this.getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
+        }
+
     }
 
     /**
